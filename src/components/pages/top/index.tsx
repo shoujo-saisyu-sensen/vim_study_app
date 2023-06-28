41e27a763fa99b0710666f17a0d99e6c32542506
takahashikosuke <takahashikosuke@team-lab.com>
2023-06-28 20:53:44 +0900

    :construction: wip

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -2,7 +2,7 @@ import styles from './style.module.scss';
 
 export const index: React.FC = () => {
   return (
-    <div>
+    <div className={styles.topWrapper}>
       <h1>Hello World</ h1>
     </ div>
   );
