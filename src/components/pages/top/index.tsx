f9baad005185d0bc357b2239913ae414461a37fc
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-02 23:19:08 +0900

    :wrench: height width

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -1,9 +1,7 @@
-import styles from './styles.module.scss';
-
 export const Top: React.FC = () => {
   return (
     <div>
-      <div className={styles.topWrapper}>
+      <div className="w-screen h-screen">
       <h1>Hello World</ h1>
     </ div>
     </div>
