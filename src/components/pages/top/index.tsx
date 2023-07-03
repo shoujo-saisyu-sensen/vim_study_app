1f7bf392ba3d134ee0013da27b136ee7dab0629c
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-03 10:25:21 +0900

    :wrench: add color

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -1,7 +1,9 @@
+import style from './styles.module.scss';
+
 export const Top: React.FC = () => {
   return (
     <div>
-      <div className="w-screen h-screen">
+      <div className={`w-screen h-screen ${style.topWrapper}`}>
       <h1>Hello World</ h1>
     </ div>
     </div>
