a411ce26f7ac5bdc80c6d08344d1785796cc0837
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-07 10:25:21 +0900

    :wrench: flex

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -3,7 +3,7 @@ import style from './styles.module.scss';
 export const Top: React.FC = () => {
   return (
     <div>
-      <div className={`w-screen h-screen flex-col ${style.topWrapper}`}>
+      <div className={`w-screen h-screen flex flex-col justify-center ${style.topWrapper}`}>
       <h1>Hello World</ h1>
       <input type="text" />
     </ div>
