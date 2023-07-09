206453aa04b30fabe0c7793f1827c53770c3c24d
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-09 11:09:10 +0900

    :wrench: padding

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -3,7 +3,7 @@ import style from './styles.module.scss';
 export const Top: React.FC = () => {
   return (
     <div>
-      <div className={`w-screen h-screen flex flex-col justify-center gap-4 ${style.topWrapper}`}>
+      <div className={`w-screen h-screen flex flex-col justify-center gap-4 p-40 ${style.topWrapper}`}>
       <h1>Hello World</ h1>
       <input type="text" />
     </ div>
