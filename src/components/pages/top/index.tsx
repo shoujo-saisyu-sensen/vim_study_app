ae6c1af186ad845913eb615029232f56fa1f6d99
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-09 22:28:10 +0900

    :wrench: add animation

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -4,9 +4,8 @@ export const Top: React.FC = () => {
   return (
     <div>
       <div className={`w-screen h-screen flex flex-col justify-center gap-4 p-40 ${style.topWrapper}`}>
-      <h1>Hello World</ h1>
-      <input type="text" />
-    </ div>
+        <input type="text" className={style.answerInput} placeholder='sample'/>
+      </ div>
     </div>
   );
 };
