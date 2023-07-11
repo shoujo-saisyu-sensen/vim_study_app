836ba8e74d5ec678b15816a7699a6e4f02017748
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-11 10:25:21 +0900

    :wrench: gap

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import style from './styles.module.scss';
 
 export const Top: React.FC = () => {
+  const [isStarted, setIsStarted] = useState<boolean>(false);
+
+  const onClickStart = () => {
+    setIsStarted(true);
+  }
+
   return (
     <div>
       <div className={`w-screen h-screen flex flex-col justify-center gap-4 p-40 ${style.topWrapper}`}>
-        <input type="text" className={style.answerInput} placeholder='sample'/>
+        {isStarted ? (<input type="text" className={style.answerInput} placeholder='sample'/>) : (<button onClick={onClickStart}>おしてね</button>)}
       </ div>
     </div>
   );
