ae1bdd60e608ed1e9422fac087840d4379d58c2c
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-22 10:25:21 +0900

    :wrench: color

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -13,7 +13,7 @@ export const Top: React.FC = () => {
       <div className={`w-screen h-screen flex flex-col justify-center gap-4 p-40 ${style.topWrapper}`}>
         {isStarted ? (<input type="text" className={style.answerInput} placeholder='sample'/>) : (
         <div className={`flex flex-col justify-center ${style.start}`}>
-          <p>START</p>
+          <p className={style.button}>START</p>
           <button onClick={onClickStart} className={style.button}>おしてね</button>
         </div>)}
       </ div>
