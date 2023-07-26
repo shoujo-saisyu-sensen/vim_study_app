5bb26db95d28af00f726c0c76b57f46d805aac20
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-26 10:25:21 +0900

    :wrench: not using tailwind

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -10,7 +10,7 @@ export const Top: React.FC = () => {
 
   return (
     <div>
-      <div className={`w-screen h-screen flex flex-col justify-center gap-4 p-40 ${style.topWrapper}`}>
+      <div className={style.topWrapper}>
         {isStarted ? (<input type="text" className={style.answerInput} placeholder='sample'/>) : (
         <div className={style.start}>
           <button onClick={onClickStart} className={style.button}>始める</button>
