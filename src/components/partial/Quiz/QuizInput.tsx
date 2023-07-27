8647766eeab3ace474443526c45e12d4a349adab
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-27 10:25:21 +0900

    :wrench: add component

---
+++ src/components/partial/Quiz/QuizInput.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import style from './styles.module.scss';
+
+export const Top: React.FC = () => {
+  return (
+    <div className={style.inputWrapper}>
+      <input type="text" className={style.input} placeholder='sample'/>
+    </div>
+  );
+};
