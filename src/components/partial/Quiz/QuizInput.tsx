87443dc415d9ae0a5738f45f7d8582805f3f56aa
takahashikosuke <takahashikosuke@team-lab.com>
2023-07-30 22:38:40 +0900

    :wrench: add mode

--- src/components/partial/Quiz/QuizInput.tsx
+++ src/components/partial/Quiz/QuizInput.tsx
@@ -1,9 +1,12 @@
 import style from './styles.module.scss';
+import { useMode } from '@/hooks/useMode';
 
 export const Top: React.FC = () => {
+  const { mode, changeMode } = useMode();
+
   return (
     <div className={style.inputWrapper}>
-      <input type="text" className={style.input} placeholder='sample'/>
+      {mode === 'answer' ? (<input type="text" className={style.input} placeholder='sample'/>) : (<div></div>)}
     </div>
   );
 };
