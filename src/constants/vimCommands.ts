971059e9d500478bb172927d31594ccdef82f03d
takahashikosuke <takahashikosuke@team-lab.com>
2023-08-04 09:33:09 +0900

    :wrench: 構造

--- src/constants/vimCommands.ts
+++ src/constants/vimCommands.ts
@@ -1,10 +1,12 @@
 export const VIM_COMMANDS: Array<{
   question: string;
+  explanation: string;
   answer: string;
   selection: Array<string>;
 }> = [
   {
     question: "文字入力にするためには？",
+    explanation: "test",
     answer: "i",
     selection: ["a", "i", "u", "e"],
   },
