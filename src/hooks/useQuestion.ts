4bfe1ada17af92df82c8408f05097b505b80e30c
takahashikosuke <takahashikosuke@team-lab.com>
2023-08-11 10:25:21 +0900

    :wrench: add question hook

---
+++ src/hooks/useQuestion.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { VIM_COMMANDS } from "@/constants/vimCommands";
+
+export const useQuestion = () => {
+  const [questionIndex, setQuestionIndex] = useState<number>(0);
+
+  const getNextCurrentQuestion = () => {
+    const targetQuestion = VIM_COMMANDS[questionIndex];
+    setQuestionIndex(questionIndex + 1);
+    return targetQuestion;
+  };
+
+  return {
+    getNextCurrentQuestion,
+  };
+};
