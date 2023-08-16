3526f667f4a73d33a63359be9440ace83f0d49e1
takahashikosuke <takahashikosuke@team-lab.com>
2023-08-16 17:36:11 +0900

    :wrench: fix work flows

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  // basePathとassetPrefixをACTIONsの場合に指定する
   basePath: process.env.GITHUB_ACTIONS && '/vim_study_app',
   assetPrefix: process.env.GITHUB_PAGES && '/vim_study_app',
   trailingSlash: true,
